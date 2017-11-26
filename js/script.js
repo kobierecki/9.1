93d7f97921ad3ba6497637fe3233e3aa807eb657
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2017-11-26 15:17:44 +0100

    Version-1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var name = prompt('Enter your name');
-//alert('Hello, ' + name);
+alert('Hello, ' + name);
 console.log('Hello, ' + name);
 /* Komentarz 1
 Komentarz 2 */
@@ -13,16 +13,16 @@ dimensions[1] = prompt('Enter second number');
 var a = dimensions[0];
 var b = dimensions[1];
 
-var triangleArea = a*b/2;
+var triangleArea = a * b / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + b + ' is equal to: ' + triangleArea);
 
-multiplicationResult = (a ** 2) - (2*a*b) + (b ** 2);
+var multiplicationResult = (a ** 2) - (2 * a * b) + (b ** 2);
 
 if (multiplicationResult > 0) {
-    console.log("Multiplication result is a plus it's "+ multiplicationResult);
+    console.log("Multiplication result is a plus it's " + multiplicationResult);
 } 
 else if (multiplicationResult < 0) {
-    console.log("Multiplication result is a minus it's "+ multiplicationResult);
+    console.log("Multiplication result is a minus it's " + multiplicationResult);
 }
 else {
     console.log('Two same numbers were defined. Result equals zero.');
