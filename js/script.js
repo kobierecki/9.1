abe50287f6121e264b8d0e3ab3caefce5728e54c
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2017-11-26 01:24:25 +0100

    Finished 8.4

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ console.log('Hello, ' + name);
 /* Komentarz 1
 Komentarz 2 */
 // Komentarz 3
+
+var dimensions = [];
+dimensions[0] = prompt('Enter first number');
+dimensions[1] = prompt('Enter second number');
+
+var triangleArea = dimensions[0]*dimensions[1]/2;
+console.log('Triangle field with base a: ' + dimensions[0] + ' and height h: ' + dimensions[1] + ' is equal to: ' + triangleArea);
