16a4143392ef18c6fa9e30951e1edc65ca64f8f0
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2017-11-29 11:44:23 +0100

    Finished 9.1

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-/* Komentarz 1
-Komentarz 2 */
-// Komentarz 3
+var a = prompt('Enter first number');
+var h  = prompt('Enter second number');
 
-var dimensions = [];
-dimensions[0] = prompt('Enter first number');
-dimensions[1] = prompt('Enter second number');
-
-var a = dimensions[0];
-var b = dimensions[1];
-
-var triangleArea = a * b / 2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + b + ' is equal to: ' + triangleArea);
-
-var multiplicationResult = (a ** 2) - (2 * a * b) + (b ** 2);
-
-if (multiplicationResult == 0) {
-    console.log('Two the same numbers were defined. Result equals zero.');
-} else  {
-    multiplicationResult > 0 ? console.log("Multiplication result is a plus it's " + multiplicationResult):console.log("Multiplication result is a minus it's " + multiplicationResult);
+function getTriangleArea(a, h) {
+    if (a > 0 && h > 0) {
+        return a * h / 2;
+    } else {
+        console.log("Nieprawidłowe dane.")
+    }  
 }
+
+var triangle1Area = console.log(getTriangleArea(10,6));
+var triangle2Area = console.log(getTriangleArea(12,3));
+var triangle3Area = console.log(getTriangleArea(40,15));
 
 
 
